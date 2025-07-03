85e2292be8f63893751ce11e63cb52b71dd2f123
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-07-03 18:19:18 -0300

    Realice el js de filtros y productos

---
+++ javascript/filtros.js
@@ -0,0 +1,50 @@
+function aplicarFiltros(){
+    const marca = document.getElementById("marca").value;
+    const sistema = document.getElementById("sistema").value;
+    const anio = document.getElementById("anio-lanzamiento").value;
+    const almacenamiento = document.getElementById("capacidad-almacenamiento").value;
+    const color = document.getElementById("color").value;
+    const precioMinimo = document.getElementById("precio-minimo").value;
+    const precioMaximo = document.getElementById("precio-maximo").value;
+
+    let filtrados = productos.filter(oProd =>{
+        return (
+            (marca === "" || oProd.marca === marca) &&
+            (sistema === "" || oProd.sistema === sistema) &&
+            (anio === "" || oProd.anio === anio)&&
+            (almacenamiento === "" || oProd.almacenamiento === almacenamiento)&&
+            (color === "" || oProd.color === color)&&
+            (precioMinimo === "" || oProd.precio >= parseInt(precioMinimo))&&
+            (precioMaximo === "" || oProd.precio <= parseInt(precioMaximo))
+        )
+    });
+    renderizarProductos(filtrados);
+}
+
+function renderizarProductos(lista){
+    const contenedor = document.getElementById("contenedor-cards");
+    contenedor.innerHTML = ""; // limpiamos resultados anteriores
+
+    if(lista.length === 0){
+        contenedor.innerHTML = "<p>No se encontraron dispositivos.</p>";
+        return;
+    };
+
+    lista.forEach(oProd => {
+        contenedor.innerHTML += `
+        <div class="col-md-3 mb-4">
+            <div class="card h-100">
+                <img src="${oProd.imagen}" class="card-img-top" alt="${oProd.modelo}">
+                <div class="card-body">
+                    <h5 class="card-title">${oProd.modelo}</h5>
+                    <p class="card-text">${oProd.marca}</p>
+                    <p class="card-text">${oProd.sistema}</p>
+                    <p class="card-text">${oProd.anio}</p>
+                    <p class="card-text">${oProd.almacenamiento}</p>
+                    <p class="card-text">${oProd.color}</p>
+                    <p class="card-text">$${oProd.precio.toLocalString()}</p>
+                </div>
+            </div>
+        </div>`
+    });
+}
---
+++ javascript/productos.js
@@ -0,0 +1,212 @@
+const productos = [
+    {
+        modelo: "iPhone 11",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "negro",
+        precio: "850000",
+        imagen: "../img/iPhonen-11-negro.jpg"
+    },
+    {
+        modelo: "iPhone 11",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "verde",
+        precio: "850000",
+        imagen: "../img/iPhonen-11-verde.jpg"
+    },
+    {
+        modelo: "iPhone 11",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "amarillo",
+        precio: "850000",
+        imagen: "../img/iPhonen-11-amarillo.jpg"
+    },
+    {
+        modelo: "iPhone 11",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "morado",
+        precio: "850000",
+        imagen: "../img/iPhonen-11-morado.jpg"
+    },
+    {
+        modelo: "iPhone SE (2da gen)",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2020",
+        almacenamiento: "64GB",
+        color: "blanco",
+        precio: "450000",
+        imagen: "../img/iPhone-SE-(2da gen)-blanco.jpg"
+    },
+    {
+        modelo: "iPhone SE (2da gen)",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2020",
+        almacenamiento: "64GB",
+        color: "negro",
+        precio: "450000",
+        imagen: "../img/iPhone-SE-(2da gen)-negro.jpg"
+    },
+    {
+        modelo: "iPhone SE (2da gen)",
+        marca: "iPhone",
+        sistema: "iOS",
+        anio: "2020",
+        almacenamiento: "64GB",
+        color: "rojo",
+        precio: "450000",
+        imagen: "../img/iPhone-SE-(2da gen)-rojo.jpg"
+    },
+    {
+        modelo: "Samsung Galaxy A54 5G",
+        marca: "Samsung",
+        sistema: "Android",
+        anio: "2023",
+        almacenamiento: "128GB",
+        color: "blanco",
+        precio: "550000",
+        imagen: "../img/Samsung-a54-blanco.jpg"
+    },
+    {
+        modelo: "Samsung Galaxy A54 5G",
+        marca: "Samsung",
+        sistema: "Android",
+        anio: "2023",
+        almacenamiento: "128GB",
+        color: "morado",
+        precio: "550000",
+        imagen: "../img/Samsung-a54-morado.jpg"
+    },
+    {
+        modelo: "Samsung Galaxy S10e",
+        marca: "Samsung",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "rosa",
+        precio: "420000",
+        imagen: "../img/Samsung-S10e-rosa.jpg"
+    },
+    {
+        modelo: "Samsung Galaxy S10e",
+        marca: "Samsung",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "amarillo",
+        precio: "420000",
+        imagen: "../img/Samsung-S10e-amarillo.jpg"
+    },
+    {
+        modelo: "Samsung Galaxy S10e",
+        marca: "Samsung",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "128GB",
+        color: "rojo",
+        precio: "420000",
+        imagen: "../img/Samsung-S10e-rojo.jpg"
+    },
+    {
+        modelo: "Xiaomi Redmi Note 13",
+        marca: "Xiaomi",
+        sistema: "Android (HyperOS)",
+        anio: "2024",
+        almacenamiento: "256GB",
+        color: "negro",
+        precio: "480000",
+        imagen: "../img/Xiaomi-Redmi-Note-13-Negro.jpg"
+    },
+    {
+        modelo: "Xiaomi Redmi Note 13",
+        marca: "Xiaomi",
+        sistema: "Android (HyperOS)",
+        anio: "2024",
+        almacenamiento: "256GB",
+        color: "azul",
+        precio: "480000",
+        imagen: "../img/Xiaomi-Redmi-Note-13-Azul.jpg"
+    },
+    {
+        modelo: "Xiaomi Redmi Note 13",
+        marca: "Xiaomi",
+        sistema: "Android (HyperOS)",
+        anio: "2024",
+        almacenamiento: "256GB",
+        color: "verde",
+        precio: "480000",
+        imagen: "../img/Xiaomi-Redmi-Note-13-Verde.jpg"
+    },
+    {
+        modelo: "Xiaomi Mi A3",
+        marca: "Xiaomi",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "64GB",
+        color: "blanco",
+        precio: "180000",
+        imagen: "../img/Xiaomi-Mi-A3-blanco.jpg"
+    },
+    {
+        modelo: "Xiaomi Mi A3",
+        marca: "Xiaomi",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "64GB",
+        color: "azul",
+        precio: "180000",
+        imagen: "../img/Xiaomi-Mi-A3-azul.jpg"
+    },
+    {
+        modelo: "Xiaomi Mi A3",
+        marca: "Xiaomi",
+        sistema: "Android",
+        anio: "2019",
+        almacenamiento: "64GB",
+        color: "negro",
+        precio: "180000",
+        imagen: "../img/Xiaomi-Mi-A3-negro.jpg"
+    },
+    {
+        modelo: "Motorola Moto G73 5G",
+        marca: "Motorola",
+        sistema: "Android",
+        anio: "2023",
+        almacenamiento: "128GB",
+        color: "rosado",
+        precio: "320000",
+        imagen: "../img/Motorola-Moto-G73-rosado.jpg"
+    },
+    {
+        modelo: "Motorola Moto G73 5G",
+        marca: "Motorola",
+        sistema: "Android",
+        anio: "2023",
+        almacenamiento: "128GB",
+        color: "azul",
+        precio: "320000",
+        imagen: "../img/Motorola-Moto-G73-azul.jpg"
+    },
+    {
+        modelo: "Motorola Moto E7 Plus",
+        marca: "Motorola",
+        sistema: "Android",
+        anio: "2020",
+        almacenamiento: "64GB",
+        color: "azul",
+        precio: "140000",
+        imagen: "../img/Motorola-Moto-E7-Plus-azul.jpg"
+    }
+]
